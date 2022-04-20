ec92f57f02c84818588e16c743011209a5fb33bb
tapia1013 <manbearpig1013@gmail.com>
2022-04-19 17:58:57 -0700

    fixed side nav with fixed postion and using next link for tv home and movie links

--- components/UI/MediaRow/MediaRow.js
+++ components/UI/MediaRow/MediaRow.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { shuffuleArray } from '../../utilities';
 import Link from 'next/link'
