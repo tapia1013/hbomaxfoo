d40c8ad297e723baee262f1a21bb04ee6f3cdd4c
tapia1013 <manbearpig1013@gmail.com>
2022-04-20 18:16:45 -0700

    completed search feature functionality and popular data

--- components/UI/SearchModal/SearchModal.js
+++ components/UI/SearchModal/SearchModal.js
@@ -1,20 +1,69 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 import { useStateContext } from "../../HBOProvider";
+import Link from 'next/link'
+import { useRouter } from 'next/router';
+
+
 
 
 
 const SearchModal = (props) => {
   const globalState = useStateContext();
 
+  const router = useRouter();
+
+  const [popData, setPopData] = useState([])
+  const [searchData, setSearchData] = useState([])
+  const [showResults, setShowResults] = useState(false)
+  const [text, setText] = useState('')
 
 
-  const loopComp = (comp, digit) => {
-    let thumbnails = [];
-    for (let index = 1; index <= digit; index++) {
-      thumbnails.push(comp)
+  useEffect(async () => {
+    try {
+      let popData = await axios.get(`https://api.themoviedb.org/3/discover/movie?primary_release_year=2022&api_key=c1b0e735ad3ff470f44fa29c9a1e6189`)
+
+      setPopData(popData.data.results.filter((item, i) => i < 14))
+
+      setShowResults(false)
+      console.log('popData ', popData.data.results)
+    } catch (error) {
+      console.log(error);
+    }
+  }, [])
+
+
+
+  const handleInput = async (e) => {
+    try {
+      setText(e.target.value)
+
+      let searchData = await axios.get(`https://api.themoviedb.org/3/search/multi?query=${e.target.value}&api_key=c1b0e735ad3ff470f44fa29c9a1e6189`)
+
+      setSearchData(searchData.data.results.filter((item, i) => item.media_type === 'tv' || item.media_type === 'movie'))
+
+      setShowResults(true)
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+
+
+  const clickedThumbnail = (type, id, media_type) => {
+    if (type === 'popular') {
+      router.push(`/movie/${id}`)
+      globalState.setSearchOpenAction(!globalState.searchOpen)
     }
 
-    return thumbnails;
+    if (type === 'search') {
+      router.push(`/${media_type}/${id}`)
+      globalState.setSearchOpenAction(!globalState.searchOpen)
+    }
+
   }
+
 
 
   return (
@@ -22,7 +71,13 @@ const SearchModal = (props) => {
       className={`search-modal ${globalState.searchOpen ? 'search-modal--active' : ''}`}
     >
       <div className="search-modal__input-group ">
-        <input className="search-modal__input" type="text" placeholder="search for a title" />
+        <input
+          className="search-modal__input"
+          type="text"
+          placeholder="search for a title"
+          onChange={handleInput}
+          value={text}
+        />
         <div
           onClick={() => globalState.setSearchOpenAction(!globalState.searchOpen)}
           className="search-modal__close-btn"
@@ -32,20 +87,63 @@ const SearchModal = (props) => {
       </div>
 
       <h3 className="search-modal__title">
-        Popular Searches
+        {
+          showResults && searchData.length >= 1
+            ? `Search Results for ${text}`
+            : 'Popular Searches'
+        }
       </h3>
 
       <div className="search-modal__thumbnails">
-
-        <div className="search-modal__thumbnail">
-          <img src="https://cdn11.bigcommerce.com/s-ydriczk/images/stencil/1280x1280/products/88997/93196/Avengers-Endgame-Final-Style-Poster-buy-original-movie-posters-at-starstills__42370.1563973516.jpg?c=2?imbypass=on" />
-          <div className="search-modal__top-layer">
-            <i className="fas fa-play" />
-          </div>
-        </div>
+        {
+          showResults && searchData.length >= 1
+            ? (<SearchResults searchData={searchData} clickedThumbnail={clickedThumbnail} />)
+            : (<PopularResults popData={popData} clickedThumbnail={clickedThumbnail} />)
+        }
       </div>
     </div>
   )
 }
+
+
+
+
+
+
+
+
+const PopularResults = (props) => {
+  return props.popData.map((item, index) => {
+    return (
+      <div key={index} className="search-modal__thumbnail" onClick={() => props.clickedThumbnail('popular', item.id)}>
+        <img src={`https://image.tmdb.org/t/p/w185${item.poster_path}`} />
+        <div className="search-modal__top-layer">
+          <i className="fas fa-play" />
+        </div>
+      </div>
+    )
+  })
+}
+
+
+
+
+
+const SearchResults = (props) => {
+  return props.searchData.map((item, index) => {
+    return (
+      <div key={index} className="search-modal__thumbnail" onClick={() => props.clickedThumbnail('popular', item.id, item.media_type)}>
+        <img src={`https://image.tmdb.org/t/p/w185${item.poster_path}`} />
+        <div className="search-modal__top-layer">
+          <i className="fas fa-play" />
+        </div>
+      </div>
+    )
+  })
+}
+
+
+
+
 
 export default SearchModal;
